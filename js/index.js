c43e2e3244b11b81eef8d7d5f528c493a4c4a307
chrispetito <christopherpetito@gmail.com>
2019-04-09 13:03:43 -0700

    add mouseover and mouseout events to title; add alert to title when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+    event.target.style.cursor = 'pointer';
+})
+logoHeading.addEventListener('mouseout', (event)=> {
+    event.target.style.color = 'black';
+})
+logoHeading.addEventListener('click', (event) => {
+    alert('Thank for Visiting Fun Bus! Subscribe for Updates!');
+})
+
+
