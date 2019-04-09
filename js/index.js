3930309d592c4f9fee7b54bd01df08faa077eead
chrispetito <christopherpetito@gmail.com>
2019-04-09 13:45:56 -0700

    add event listeners for nav anchors and destination buttons; add input fields; add preventDefault for nav anchors

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Your code goes here
+
 let logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseover', (event) => {
     event.target.style.color = 'blue';
@@ -11,4 +12,53 @@ logoHeading.addEventListener('click', (event) => {
     alert('Thank for Visiting Fun Bus! Subscribe for Updates!');
 })
 
+//Victor's event listenters
+let button1 = document.querySelectorAll('.destination .btn')[0]
+button1.addEventListener('click', (event) => {
+    button1.style.background = 'purple'
+    button1.innerHTML = 'Pop!'
+})
 
+ let button2 = document.querySelectorAll('.destination .btn')[1]
+button2.addEventListener('dblclick', (event) => {
+    button2.style.background = 'blue'
+    button2.innerHTML = 'Double Click!'
+})
+
+let button3 = document.querySelectorAll('.destination .btn')[2]
+button3.addEventListener('mouseenter', (event) => {
+    button3.style.background = 'green'
+    button3.innerHTML = 'Green Version'
+  })
+  button3.addEventListener('mouseleave', (event) => {
+    button3.style.background = '#17A2B8'
+    button3.innerHTML = 'Sign Me Up!'
+  })
+
+//Skylar's event listeners
+const linksin = document.querySelectorAll('a');
+for (var i=0; i < linksin.length; i++) {
+   linksin[i].addEventListener('mouseover', (event) => {
+       event.target.style.color = 'blue'
+       // console.log("target:", event.target);
+       });
+}
+const linksout = document.querySelectorAll('a');
+for (var i=0; i < linksout.length; i++) {
+   linksout[i].addEventListener('mouseout', (event) => {
+       event.target.style.color = 'black'
+       // console.log("target:", event.target);
+   });}
+
+window.addEventListener('load', (event) => {
+       alert("The Page Loaded");
+});
+
+//prevent default for nav items
+const links = document.querySelectorAll('a');
+for (var i=0; i < links.length; i++) {
+   linksin[i].addEventListener('click', (event) => {
+       event.preventDefault();
+       // prevent default refresh on anchors
+       });
+}
